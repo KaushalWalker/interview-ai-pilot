d089b06d1d110702d5a5fff16a1b9a0782c1cf61
KaushalWalker <kaushal.jambhle@gmail.com>
2026-07-22 09:10:39 +0530

    Improve PostgreSQL connection handling

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -7,7 +7,8 @@ from app.core.config import settings
 engine = create_engine(
     settings.DATABASE_URL,
     echo=True,
-)
+    pool_pre_ping=True   # This tell Sqlalchemy before using an existing database connection 
+)                        # check whether it's still alive. if it's dead reconnect insted of uisng the broken connection.
 
 SessionLocal = sessionmaker(
     autocommit=False,
